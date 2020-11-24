34a92eac500798f00e04d7af7684725957c58bc0
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-24 17:13:38 -0500

    changes to package.json removed engine

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Route working!');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
