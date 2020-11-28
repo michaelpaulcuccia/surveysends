24da1f8c2677b185ae828c608e927c6433797a53
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-28 18:01:53 -0500

    proxy G+ button

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,24 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      Hello, from App.js
+      <div
+        className='sign_in'
+        style={{
+          backgroundColor: 'red',
+          width: '3rem',
+          height: '3rem'
+        }}>
+        <a href="/auth/google">
+          <i className='fab fa-google-plus-g red'
+            style={{
+              color: 'white',
+              width: '50%',
+              padding: '15% 10% 15% 10%',
+              fontSize: '30px'
+            }}>
+          </i>
+        </a>
+      </div>
     </div>
   )
 }
