5d6813d280cffa2b133be08bddb6c5a1a327cbf4
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-28 10:12:23 -0500

    updated documentation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,20 +6,17 @@ module.exports = (app) => {
     app.get('/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
-        }),
-        (req, res) => {
-
-        }
+        })
     );
 
     //callback
     app.get('/auth/google/callback',
         passport.authenticate('google')
-
     );
 
     //get access to user
     app.get('/api/current_user', (req, res) => {
+        //console.log(req.session) //contains data passport is attempting to store in cookie (mongodb id), passed to deserialize user
         res.send(req.user)
     });
 
