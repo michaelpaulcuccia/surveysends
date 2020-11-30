fc8ba19737dc5327048730f07e37bfa666c7afe7
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-11-30 10:11:30 -0500

    implemented redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <div
-        className='sign_in'
-        style={{
-          backgroundColor: 'red',
-          width: '3rem',
-          height: '3rem'
-        }}>
-        <a href="/auth/google">
-          <i className='fab fa-google-plus-g red'
-            style={{
-              color: 'white',
-              width: '50%',
-              padding: '15% 10% 15% 10%',
-              fontSize: '30px'
-            }}>
-          </i>
-        </a>
-      </div>
+      Hello, from App.js
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+
+//dummy reducer - arrow function that returns an array
+//2nd arg is initial state of app
+const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
