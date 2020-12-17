bf1aa3be9f068ad48a33316b05291c9cf76b7cac
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-17 15:25:20 -0500

    updated Payment style

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,15 +5,23 @@ const Payments = () => {
 
     //amount = cents
     //token = callback function, called after successfully retrievinig auth token from stripe api
+    //name = header
+    //description = header
+
+    //button child component utilizes material-css instead of built-in styles
 
     return (
 
         <div>
             <StripeCheckout
+                name="Survey_Sends"
+                description="$5 for 5 Credits"
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
-            />
+            >
+                <button className='btn'>Add Credits</button>
+            </StripeCheckout>
         </div>
     )
 }
