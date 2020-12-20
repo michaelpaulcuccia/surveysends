37b8bff8d2d06a1154bbe904386ed0bade07d8f7
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-12-20 17:21:58 -0500

    updated model for credits, billingRoute updating. video not the same

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,23 +1,43 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 module.exports = (app) => {
 
     app.post('/api/stripe', async (req, res) => {
 
         //console.log(req.body)
+        //user - passport de/serializeUser() has access to MongoDB id
+
+        const { id } = req.body;
+
+        console.log('///---///')
 
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
-            source: req.body.id
+            source: id
         });
 
-        console.log(charge)
+        //get MongoDB Id
+        let userId = req.user._id
+
+        //find user by MongoDB Id
+        let user = await User.findOne({ _id: userId });
+
+        if (user) {
+            user.credits += 5;
+
+            const userUpdated = await user.save();
+            res.send(userUpdated);
+        }
+
 
     });
 
 };
 
 //NOTE: Returns a function, immediatly invoke function with app as arg
+
